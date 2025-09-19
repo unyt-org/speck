a96cd50a54f954b6c815c223b5995eae69501508
Benedikt Strehle <benedikt@unyt.org>
2025-09-19 21:54:52 +0200

    fix npm build

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -23,7 +23,6 @@ await build({
     },
     typeCheck: "both",
     scriptModule: false,
-    test: false, // TODO: enable, currently fails, see https://github.com/denoland/dnt/issues/249
 
     package: {
         // package.json properties
@@ -32,33 +31,14 @@ await build({
         license: "MIT",
         repository: {
             type: "git",
-            url: "git+https://github.com/unyt-org/datex-core-js.git",
+            url: "git+https://github.com/unyt-org/speck.git",
         },
         bugs: {
-            url: "https://github.com/unyt-org/datex-core-js/issues",
+            url: "https://github.com/unyt-org/speck/issues",
         },
     },
     // steps to run after building and before running the tests
     postBuild() {
-        // replace import.meta because dnt-shim-ignore does not work here
-        const datexCoreJSInternalPath = new URL(
-            "../npm/esm/datex-core/datex_core_js.js",
-            import.meta.url,
-        );
-        const fileContent = Deno.readTextFileSync(datexCoreJSInternalPath);
-        const updatedContent = fileContent.replace(
-            `globalThis[Symbol.for("import-meta-ponyfill-esmodule")](import.meta).url`,
-            `import.meta.url`,
-        );
-        Deno.writeTextFileSync(datexCoreJSInternalPath, updatedContent);
-
         Deno.copyFileSync("README.md", "npm/README.md");
-        Deno.copyFileSync(
-            "src/datex-core/datex_core_js.wasm",
-            "npm/esm/datex-core/datex_core_js.wasm",
-        );
-
-        // currently required for version tests
-        Deno.copyFileSync("deno.json", "npm/esm/deno.json");
     },
 });
