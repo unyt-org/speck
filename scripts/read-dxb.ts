d2b320f0f3944a0b84cd242529cc05021ca3b7f9
Jonas Strehle <jonas@strehles.info>
2025-10-01 16:40:41 +0200

    add read dxb script

---
+++ scripts/read-dxb.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S deno run --allow-read
+
+import { parseAndPackStructure } from '../packer.ts';
+import dxbStructure from '../examples/dxb.json' with { type: "json" };
+import type { StructureDefinition } from "../types.ts";
+if (Deno.args.length !== 1) {
+  console.error("Usage: ./read-dxb.ts <file>");
+  Deno.exit(1);
+}
+
+const relativePath = Deno.args[0];
+const absolutePath = new URL(relativePath, `file://${Deno.cwd()}/`).pathname;
+
+if (!Deno.stat(absolutePath)) {
+  console.error(`Error: File not found -> ${absolutePath}`);
+  Deno.exit(1);
+}
+const data = Deno.readFileSync(absolutePath);
+const packed = parseAndPackStructure(dxbStructure as StructureDefinition, data);
+console.dir(packed, { depth: null });
